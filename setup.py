cd05237758fc9b2e7dc6519321f757205ee1a916
Sean Brant <brant.sean@gmail.com>
2012-07-09 19:59:54 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='virtualenvwrapper.configvar',
+    version='0.0.1',
+    description='Plugin for virtualenvwrapper to automatically '
+                'export config vars found in your project level '
+                '.env file.',
+    author='Sean Brant',
+    author_email='brant.sean@gmail.com',
+    url='https://github.com/seanbrant/virtualenvwrapper.configvar',
+    namespace_packages=['virtualenvwrapper'],
+    packages=['virtualenvwrapper'],
+    install_requires=[
+        'virtualenv',
+        'virtualenvwrapper>=2.11',
+    ],
+    entry_points={
+        'virtualenvwrapper.post_activate': [
+            'configvars = virtualenvwrapper.configvars:post_activate',
+        ],
+        'virtualenvwrapper.post_activate_source': [
+            'configvars = virtualenvwrapper.configvars:post_activate_source',
+        ],
+        'virtualenvwrapper.post_deactivate': [
+            'configvars = virtualenvwrapper.configvars:post_deactivate',
+        ],
+        'virtualenvwrapper.post_deactivate_source': [
+            'configvars = virtualenvwrapper.configvars:post_deactivate_source',
+        ],
+    }
+)
---
+++ virtualenvwrapper/configvars.py
@@ -0,0 +1,84 @@
+import logging
+import os
+
+
+log = logging.getLogger(__name__)
+
+
+def trim(*values):
+    return [value.strip() for value in values]
+
+
+class Loader(object):
+    _old_prefix = '__OLD_'
+    _exported_key = '__EXPORTED'
+
+    def __init__(self):
+        self._exported = []
+        self._source_lines = []
+
+    def load(self, path):
+        try:
+            with open(path, 'r') as env:
+                self._parse(env.read())
+        except IOError:
+            log.info('Skip loading environment. %s does not exist.' % path)
+
+    def clear(self):
+        for key, value in os.environ.items():
+            if key.startswith(self._old_prefix):
+                self._unset(key)
+                self._export(''.join(key.split(self._old_prefix)[1:]), value)
+
+        for key in os.environ.get(self._exported_key, '').split(','):
+            if key:
+                self._unset(key)
+
+        self._unset(self._exported_key)
+
+    def source(self):
+        if self._exported:
+            self._export(self._exported_key, ','.join(self._exported))
+
+        content = '\n'.join(self._source_lines)
+
+        self._source_lines = []
+        self._exported = []
+
+        return content
+
+    def _parse(self, content):
+        lines = content.splitlines()
+        pairs = [trim(*line.split('=')) for line in lines if line.strip()]
+
+        for key, value in pairs:
+            if key in os.environ:
+                self._export('%s%s' % (self._old_prefix, key), os.environ[key])
+
+            self._export(key, value)
+
+    def _export(self, key, value):
+        self._exported.append(key)
+        self._source_lines.append('export %s=%s' % (key, value))
+
+    def _unset(self, key):
+        self._source_lines.append('unset %s' % key)
+
+
+loader = Loader()
+
+
+def post_activate(args):
+    loader.load(os.path.join(os.environ['OLDPWD'], '.env'))
+
+
+def post_activate_source(args):
+    return loader.source()
+
+
+def post_deactivate(args):
+    loader.clear()
+
+
+def post_deactivate_source(args):
+    return loader.source()
